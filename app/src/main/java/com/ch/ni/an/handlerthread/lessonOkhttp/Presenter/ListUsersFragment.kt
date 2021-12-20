f51cd9bfa571d5642d6a62d08318b81954a7fc38
Nikita <cherskih2@gmail.com>
2021-12-20 13:44:03 +1000

    koil

--- app/src/main/java/com/ch/ni/an/handlerthread/lessonOkhttp/Presenter/ListUsersFragment.kt
+++ app/src/main/java/com/ch/ni/an/handlerthread/lessonOkhttp/Presenter/ListUsersFragment.kt
@@ -2,13 +2,9 @@ package com.ch.ni.an.handlerthread.lessonOkhttp.Presenter
 
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.fragment.app.*
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.RecyclerView
 import com.ch.ni.an.handlerthread.R
 import com.ch.ni.an.handlerthread.databinding.FragmentUsersBinding
 import com.ch.ni.an.handlerthread.lessonOkhttp.Presenter.adapters.UsersAdapter
@@ -17,33 +13,25 @@ import com.ch.ni.an.handlerthread.lessonOkhttp.Presenter.viewModels.ListUserView
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
-class ListUsersFragment : Fragment() {
+class ListUsersFragment : Fragment(R.layout.fragment_users) {
 
     private var _binding: FragmentUsersBinding? = null
+
     private val binding get() = _binding!!
 
     private val myModel by viewModel<ListUserViewModel>()
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: UsersAdapter
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-
-        _binding = FragmentUsersBinding.inflate(inflater, container, false)
-        return binding.root
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentUsersBinding.bind(view)
 
-        recyclerView = binding.recyclerView
-        adapter = UsersAdapter {
+        val recyclerView = binding.recyclerView
+
+        val adapter = UsersAdapter {
             val user: UserUiModel = it.tag as UserUiModel
             showUserDetail(user)
         }
+
         recyclerView.adapter = adapter
 
         myModel.users.observe(viewLifecycleOwner, {
--- app/src/main/java/com/ch/ni/an/handlerthread/lessonOkhttp/di/DataModule.kt
+++ app/src/main/java/com/ch/ni/an/handlerthread/lessonOkhttp/di/DataModule.kt
@@ -5,8 +5,6 @@ import data.okHttp.OkHttp
 import data.repository.RepositoryImpl
 import data.retrofit.Common
 import data.retrofit.RetrofitApi
-import domain.repository.FetchListUsers
-import domain.repository.PostsRepository
 import domain.repository.Repository
 import org.koin.dsl.module
 
